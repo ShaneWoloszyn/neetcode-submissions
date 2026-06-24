2fd9eaa16abe4672730eafcfd3735901d89911ad
ShaneWoloszyn <114633387+ShaneWoloszyn@users.noreply.github.com>
2026-06-24 10:47:27 -0400

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
+        res = []
+        subset = []
+
+        dMap = {2:["a", "b", "c"], 
+                3:["d", "e", "f"],
+                4:["g", "h", "i"],
+                5:["j", "k", "l"],
+                6:["m", "n", "o"],
+                7:["p", "q", "r", "s"],
+                8:["t", "u", "v"],
+                9:["w", "x", "y", "z"]}
+        
+        def search(i):
+            print(subset)
+            if i == len(digits):
+                res.append("".join(subset))
+                return
+
+            letters = dMap[int(digits[i])]
+
+            for char in letters:
+                subset.append(char)
+                search(i + 1)
+                subset.pop()
+        
+        search(0)
+        return res
