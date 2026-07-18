24590880a15240b4731f411376f1ada512c4e522
ShaneWoloszyn <114633387+ShaneWoloszyn@users.noreply.github.com>
2026-07-18 14:25:55 -0400

    Add: valid-sudoku - submission-7

---
+++ Data Structures & Algorithms/valid-sudoku/submission-7.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        def check(nums):
+            nMap = {}
+            for n in nums:
+                if n != "." and n in nMap:
+                    return False
+                nMap[n] = 1
+            
+            return True
+        
+        for rows in board:
+            if not check(rows):
+                return False
+        
+        for i in range(9):
+            col = []
+            for j in range(9):
+                col.append(board[j][i])
+            if not check(col):
+                return False
+        
+        for i in range(3):
+            for j in range(3):
+                square = []
+                for k in range(3):
+                    for l in range(3):
+                        square.append(board[i * 3 + k][j * 3 + l])
+                
+                if not check(square):
+                    return False
+        
+        return True
